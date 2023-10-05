9e7fa235278628dce21033988ffd64ac6c09544e
Ikechukwu Celestine <devikecele@gmail.com>
2023-10-05 13:44:23 +0100

    JavaScript function that checks for a color and returns the name and index

---
+++ JS-Exercises/6-ColourChecker.js
@@ -0,0 +1,13 @@
+function colorChecker(color_arr, color) {
+    for (let i = 0; i < color_arr.length; i++) {
+        return (color_arr[i] === color)
+        ? color_arr[i] + " : " + i
+        : "Color not found! Pls, try another color name";
+    }
+}; // JavaScript function that helps to check for colors
+
+const myColor = ["red", "blue", "green", "yellow", "black", "purple", "white", "grey", "cyan"]; // myColor array
+
+colorChecker(myColor, "red"); // Calling the colorChecker function with the following parameters
+
+console.log(myColor); // This will console.log the function
