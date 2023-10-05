f95193d7228e483440a6a308af7c7eae9f405174
Ikechukwu Celestine <devikecele@gmail.com>
2023-10-05 13:10:01 +0100

    JavaScript function for finding Power of a number

--- JS-Exercises/1-RaiseToPower.js
+++ JS-Exercises/1-RaiseToPower.js
@@ -1,5 +1,5 @@
 function raiseToPower(base, exp) {
   return Math.pow(base, exp);
-};
+}; // JavaScript function that shows the value of a Power
 
 console.log(raiseToPower(4, 2)); // This will console.log the function
