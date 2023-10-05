1b93509fa62cfa93fdbb1ffd634ef909527d082e
Ikechukwu Celestine <devikecele@gmail.com>
2023-10-05 13:27:12 +0100

    JavaScript function for finding a minimum value

--- JS-Exercises/5-MinValue.js
+++ JS-Exercises/5-MinValue.js
@@ -4,6 +4,6 @@ function min(a, b) {
     } else {
         return b;
     }
-}; // JavaScript function to find the minimum value
+}; // JavaScript function to find the minimum value between two numbers
 
 console.log(min(7, 9)); // This will console.log the function
