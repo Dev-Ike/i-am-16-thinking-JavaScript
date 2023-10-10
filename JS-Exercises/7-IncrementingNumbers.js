6f5dc534e234b6fd294a37c849a824d6a8c0d4c9
Ikechukwu Celestine <devikecele@gmail.com>
2023-10-10 12:24:59 +0100

    JavaScript function that increment numbers using the map() method

--- JS-Exercises/7-IncrementingNumbers.js
+++ JS-Exercises/7-IncrementingNumbers.js
@@ -3,4 +3,4 @@ const arr = [1, 2, 3, 4, 5, 6 , 7]; // This is an array of numbers
 const incrementedArr = arr.map((element) => element + 1); // This iterates through the array using the map() method to increment all the numbers in the array by 1
 
 console.log("before : ", arr); // This console.log the array
-console.log("after : ", incrementedArr); // This console.log the incremented array
+console.log("after : ", incrementedArr); // This will console.log the incremented array
