44ca5aa4babc0eba6b0fa04fbad2ea14890344c2
Ikechukwu Celestine <devikecele@gmail.com>
2023-10-10 12:05:29 +0100

    JavaScript function that converts Celsius to Fahrenheit

---
+++ JS-Exercises/8-ConvertCtoF.js
@@ -0,0 +1,6 @@
+function convertCtoF(celsius) {
+  let fahrenheit = celsius * 9/5 + 32;
+  return fahrenheit;
+}; // JavaScript function that converts Celsius to Fahrenheit
+
+console.log(convertCtoF(45)); // This will console.log the function
